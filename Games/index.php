360693c2a4702ef827705ce23f788f2c1e4c2712
Dennisonung <Dennisonung@gmail.com>
2022-03-08 10:03:06 -0500

    pov php moment

--- Games/index.php
+++ Games/index.php
@@ -27,18 +27,22 @@
 				$html = '
                         <div class="col-lg-4 fade-in gamebox ">
                             <div class="bg-black2 box-container-shadow mt-4 pb-2 gamebox2">
-                                <img src="/Games/Lists/' . $metadata["id"] . '/image.jpg" style="width: 100%" class="center"></img>
-                                <h3><a href="' . $metadata["id"] . '/"></h3>
-                                <h3 style="text-align:center;">' . $metadata['name'] . '</a></h3>
-								<div class="row center" style="width:90%; position: absolute; bottom:0; padding-right:1.1rem">
-                                	<a href="' . $metadata["id"] . '/" class="btn rred gameboxbutton" style="width:27%;margin-left: 1.3rem !important;">Info</a>
-									<a href="' . $metadata["Steam"] . '/" class="btn rblue gameboxbutton" style="width:29%;">Steam</a>';
-				if (isset($metadata['Mobile'])) {
-					$html = $html .  '<a href="' . $metadata["Mobile"] . '/" class="btn rgreen gameboxbutton" style="width:29%;text-align:center">Mobile</a>';
-				}
-				if (isset($metadata['PlayWebGL'])) {
-					$html = $html .  '<a href="' . $metadata["PlayWebGL"] . '/" class="btn rorange gameboxbutton" style="width:29%;text-align:center">Play on Web</a>';
-				}
+                                <img src="/Games/Lists/' . $metadata["id"] . '/image.jpg" style="width: 100%"></img>
+                                <h3 style="text-align:center;"><a href="' . $metadata['id'] .'">' . $metadata['name'] . '</a></h3>
+								<div class="row" style="width:90%;margin-left:5%;margin-right:5%; position: absolute; bottom:0; ">
+                                	<a href="' . $metadata["id"] . '/" class="btn rred gameboxbutton" style="width:90%;">Info</a>';
+									if (isset($metadata['itch'])) {
+										$html = $html . '<a href="' . $metadata["itch"] . '/" class="btn rblue gameboxbutton" style="width:29%;">itch.io</a>';
+									}
+									if (isset($metadata['Steam'])) {
+										$html = $html . '<a href="' . $metadata["Steam"] . '/" class="btn rblue gameboxbutton " style="width:29%;">Steam</a>';
+									}
+									if (isset($metadata['Mobile'])) {
+										$html = $html .  '<a href="' . $metadata["Mobile"] . '/" class="btn rgreen gameboxbutton " style="width:29%;text-align:center">Mobile</a>';
+									}
+									if (isset($metadata['PlayWebGL'])) {
+										$html = $html .  '<a href="' . $metadata["PlayWebGL"] . '/" class="btn rorangesoda gameboxbutton " style="width:29%;text-align:center">WebGL</a>';
+									}
 				$html = $html . '
 								</div>
                             </div>
@@ -50,6 +54,6 @@
     </div>
 </body>
 <br>
-<?php include("../res/includes/footer.php"); ?>
+<?php include($root . "/res/includes/footer.php"); ?>
 
 </html>
