17abed2589ab46906db9c829f548bd2468b815e3
Dennisonung <Dennisonung@gmail.com>
2022-07-02 17:41:41 -0400

    New Game no way

---
+++ Games/BeansOnDuty/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="full-height" lang="en">
+	<head>
+		<?php $root = $_SERVER['DOCUMENT_ROOT'] ?>
+		<?php include($root."/res/includes/header.php"); ?>
+		<title>Scuffed Development</title>
+	</head>
+
+<body class="background full-height w-100 bg-black">
+    <br>
+    <h1 class="cover-heading gradient-rainbow" style="text-align: center">Beans On Duty</h1>
+	<h1 class="cover-heading gradient-rainbow" style="text-align: center; color:darkgoldenrod">THIS GAME IS IN DEVELOPMENT.</h1>
+</body>
+
+</html>
