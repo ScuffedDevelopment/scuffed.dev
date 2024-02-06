f7ec6880b6c126b8b9fd08a68750dbc9a09fefc3
Dennisonung <Dennisonung@gmail.com>
2024-02-06 11:19:13 -0500

    d

--- Games/ScuffedBhop2029/index.php
+++ Games/ScuffedBhop2029/index.php
@@ -8,7 +8,7 @@
 
 <body class="background full-height w-100 bg-black">
     <br>
-    <h1 class="cover-heading gradient-rainbow" style="text-align: center">Scuffed Party (Retired)</h1>
+    <h1 class="cover-heading gradient-rainbow" style="text-align: center">*NEW* FINAL SCUFFED WITH A LITTLE POLISH BHOP EXPERIENCE 2029 BRAIN ROT EDITION (POG CHAMP)</h1>
 </body>
 
 </html>
